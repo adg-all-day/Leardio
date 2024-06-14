6d05b18c562b5f54b006d44e833277af27c8d009
Tomiwa <tomadeaga@gmail.com>
2024-06-14 22:24:15 +0100

    linked homepage to upload page

---
+++ convertor/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+# convertor/views.py
+
+from django.shortcuts import render
+
+def landing_page(request):
+    return render(request, 'pages/home.html')
+
+def upload_video(request):
+    if request.method == 'POST':
+        # Handle file upload
+        pass
+    return render(request, 'pages/upload.html')
